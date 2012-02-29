3388d50eaaa23db47ac7d7495fee5e4b0ab6c835
Romuald Quantin <romu@soundstep.com>
2012-02-29 22:13:07 +0000

    AppNote demo update for IE7.

--- demos/SomaCoreJsAppNote/js/app/views/note_list_view.js
+++ demos/SomaCoreJsAppNote/js/app/views/note_list_view.js
@@ -6,13 +6,15 @@ var NoteListView = soma.View.extend({
 			var li = document.createElement("li");
 			var title = document.createTextNode(list[i].title);
 			li.appendChild(title);
-			li.addEventListener("click", this.clickHandler, false);
+			// it is not possible without hacks to dispatch custom event from a DOM element with IE7 and IE8
+			// the variable "self" keeps a reference to the view (soma.View) so an event can be dispatched from
+			var self = this;
+			utils.addEventListener(li, "click", function() {
+				self.dispatchEvent(new NoteEvent(NoteEvent.EDIT, null, this.textContent ? this.textContent : this.innerText));
+			}.bind(li));
 			ul.appendChild(li);
 		}
 		this.domElement.appendChild(ul);
-	},
-	clickHandler: function() {
-		this.dispatchEvent(new NoteEvent(NoteEvent.EDIT, null, this.textContent));
 	},
 	show: function() {
 		this.domElement.style.display = "block";

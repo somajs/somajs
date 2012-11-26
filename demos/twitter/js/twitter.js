55e692f29bb287a2e488d6f2eed65ad4e0f96d8f
Romuald Quantin <romu@soundstep.com>
2012-11-26 16:11:34 +0000

    twitter demo update

--- demos/twitter/js/twitter.js
+++ demos/twitter/js/twitter.js
@@ -1,20 +1,22 @@
-;(function (ns, undefined) {
+;(function (twitter, undefined) {
 
+	// application
 	var App = soma.Application.extend({
 		init:function () {
+			this.createPlugin(soma.template.plugin);
 			this.injector.mapClass('service', TwitterService, true);
 			this.commands.add(Events.SEARCH, SearchCommand);
-			this.mediators.create(MediatorInput, $('.twitter .queryInput'));
-			this.mediators.create(MediatorMessage, $('.twitter .message'));
-			this.mediators.create(MediatorResult, $('.twitter .result'));
 		}
 	});
 
+	// event types
 	var Events = {
 		"SEARCH": "search",
 		"SEARCH_RESULT": "search_result"
 	}
 
+	// command that triggers the search
+	// can be used from anywhere
 	var SearchCommand = function (service) {
 		return {
 			execute:function (event) {
@@ -23,6 +25,8 @@
 		}
 	};
 
+	// service that retrieve the list of tweets from the twitter API
+	// dispatches a search result events, can be listened to from anywhere
 	var TwitterService = function (dispatcher) {
 		var url = "http://search.twitter.com/search.json";
 		return {
@@ -39,8 +43,36 @@
 		}
 	};
 
+	// template to display the list of tweets
+	var TwitterTemplate = function(scope, template, element, mediators, dispatcher) {
+		// create a mediator to handle the output
+		// optional task but for the sake of the example
+		mediators.create(MediatorInput, $('.queryInput', element));
+		// register listeners to handle search and search results events
+		dispatcher.addEventListener(Events.SEARCH, searchHandler);
+		dispatcher.addEventListener(Events.SEARCH_RESULT, resultHandler);
+		// handle search event, change message
+		function searchHandler(event) {
+			scope.message = "Searching...";
+			template.render();
+		}
+		// handle search result event, change message and update template repeater
+		function resultHandler(event) {
+			scope.tweets = event.params.results;
+			scope.message = "Search result: " + scope.tweets.length;
+			template.render();
+		}
+		// open a new window to the selected tweet
+		$(element).on('click', 'li', function() {
+			window.open("http://twitter.com/" + $(this).attr('data-user') + "/statuses/" + $(this).attr('data-id-str'));
+		});
+	};
+
+	// mediator that handle the text input
 	var MediatorInput = function (target, dispatcher) {
+		// set focus on the text input
 		setTimeout(function () { $(target).focus(); }, 50);
+		// handle keyboard event (ENTER)
 		$(target).keypress(function (event) {
 			if (event.keyCode === 13 && this.value !== "") {
 				dispatcher.dispatch(Events.SEARCH, this.value);
@@ -48,40 +80,10 @@
 		});
 	};
 
-	var MediatorMessage = function (target, dispatcher) {
-
-		dispatcher.addEventListener(Events.SEARCH, searchHandler);
-		dispatcher.addEventListener(Events.SEARCH_RESULT, resultHandler);
-
-		function searchHandler(event) {
-			$(target).html("Searching...");
-		}
-
-		function resultHandler(event) {
-			$(target).html("Search result: " + event.params.results.length);
-		}
-	};
-
-	var MediatorResult = function (target, dispatcher) {
-
-		$(target).on('click', 'li', clickHandler);
-
-		dispatcher.addEventListener(Events.SEARCH_RESULT, resultHandler);
-
-		function resultHandler(event) {
-			var list = $('ul', target).html("");
-			$(event.params.results).each(function (i, el) {
-				list.append('<li data-id-str="' + el.id_str + '" data-user="' + el.from_user + '"><div class="tpl-tweet"><img src="' + el.profile_image_url + '" alt=""/><div class="tpl-tweet-username">' + el.from_user + '</div><div class="tpl-tweet-text">' + el.text + '</div></div></li>');
-			});
-		}
-
-		function clickHandler(event) {
-			var user = $(this).attr('data-user');
-			var id = $(this).attr('data-id-str');
-			window.open("http://twitter.com/" + user + "/statuses/" + id);
-		}
-	};
-
+	// create application
 	var app = new App();
 
-})(this['ns'] = this['ns'] || {});
+	// exports
+	twitter.TwitterTemplate = TwitterTemplate;
+
+})(this['twitter'] = this['twitter'] || {});

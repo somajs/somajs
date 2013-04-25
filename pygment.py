3bed30ee3ae1de06ddb03d750f75f9ba025e1ac6
Romuald Quantin <romu@soundstep.com>
2013-04-25 17:30:08 +0100

    dependecy injection dic in progress

--- pygment.py
+++ pygment.py
@@ -3,19 +3,7 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<script>
-var Terran = function(unit, character, world, weapon) {
-
-};
-// create a terran without dependency injection
-var terran = new Terran(
-  new MarineUnit(new HumanLife(), new HumanBehavior(), new HumanResistance(), new HumanUtilities()),
-  new JimRaynorCharacter(new JimRaynorHistory()),
-  new BraxisWorld(new Weather()),
-  new LaserWeapon(new LaserBullet(), new LaserResistance())
-);
-// create a terran with dependency injection
-var terran = injector.createInstance(Terran);
-
+this.injector.mapClass("config", Config, true);
 </script>
 """
 

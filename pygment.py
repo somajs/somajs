b936d4023bf0d967c59f95a08638c0f8bedf1ce6
Romuald Quantin <romu@soundstep.com>
2013-04-25 18:27:19 +0100

    dependecy injection doc in progress

--- pygment.py
+++ pygment.py
@@ -3,7 +3,26 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<script>
+var Config = function() {
+  console.log("Config instantiated");
+};
+
+var Model = function(config) {
+  console.log("Model instantiated with a config injected:", config);
+};
+
+var Command = function(model) {
+  this.execute = function() {
+    console.log("Command executed with a model injected:", model);
+  }
+};
+
+// mapping rule: map the string "config" to the function Config
 this.injector.mapClass("config", Config, true);
+// mapping rule: map the string "model" to the function Model
+this.injector.mapClass("model", Model, true);
+// command mapped to an event
+this.commands.add("some-event", Command);
 </script>
 """
 

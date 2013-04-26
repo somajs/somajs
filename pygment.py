4b8166cf58c8571520e92592c95debd0b51c5a88
Romuald Quantin <romu@soundstep.com>
2013-04-26 11:42:59 +0100

    dispatcher doc in progress

--- pygment.py
+++ pygment.py
@@ -3,26 +3,8 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<script>
-var Config = function() {
-  console.log("Config instantiated");
-};
-
-var Model = function(config) {
-  console.log("Model instantiated with a config injected:", config);
-};
-
-var Command = function(model) {
-  this.execute = function() {
-    console.log("Command executed with a model injected:", model);
-  }
-};
-
-// mapping rule: map the string "config" to the function Config
-this.injector.mapClass("config", Config, true);
-// mapping rule: map the string "model" to the function Model
-this.injector.mapClass("model", Model, true);
-// command mapped to an event
-this.commands.add("some-event", Command);
+dispatcher.dispatchEvent(new soma.Event('some-event'));
+dispatcher.dispatchEvent(new soma.Event('some-event', {data:"some parameters"}));
 </script>
 """
 

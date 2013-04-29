812f2e1017bbff32f3a6ca5e08b7a1f87ab75c2a
Romuald Quantin <romu@soundstep.com>
2013-04-29 19:43:33 +0100

    oop doc in progress

--- pygment.py
+++ pygment.py
@@ -2,37 +2,10 @@ from pygments import highlight
 from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<script>
-var EditorModel = function(dispatcher) {
-  dispatcher.addEventListener('logout', function(event) {
-    console.log("Logout event received in EditorModel");
-    if (!this.fileSaved) {
-      console.log('Interupt logout command');
-      event.preventDefault();
-    }
-  });
-};
-
-var LogoutCommand = function() {
-  this.execute = function(event) {
-    console.log("Logout user");
-  }
-};
-
-var EditorApplication = soma.Application.extend({
-  init: function() {
-    this.commands.add("logout", LogoutCommand);
-    this.injector.createInstance(EditorModel);
-  },
-  start: function() {
-    // dispatch a cancelable event
-    // dispatch(eventType, parameters, bubbles, cancelable)
-    this.dispatcher.dispatch("logout", null, false, true);
-  }
-});
-
-var app = new EditorApplication();
-</script>
+code = """<div class="app">
+  <h1>{{content.title}}</h1>
+  <div data-repeat="item in content.items">{{$index}} - {{item}}</div>
+</div>
 """
 
 print highlight(code, HtmlDjangoLexer(), HtmlFormatter())

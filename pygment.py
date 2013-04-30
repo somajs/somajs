a937d3c97e9514b0e47b9096911719e21593badf
Romuald Quantin <romu@soundstep.com>
2013-04-30 18:46:30 +0100

    ready plugin added

--- pygment.py
+++ pygment.py
@@ -3,65 +3,32 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<script>
-// plugin to retrieve the mouse coordinates
-var OrientationPlugin = function(dispatcher) {
-  // hold current orientation
-  var orientation = detectDeviceOrientation();
-  // add listener to detect orientation change
-  window.addEventListener('orientationchange', handler);
-  // store the new orientation and dispatch an event
-  function handler(event) {
-    orientation = detectDeviceOrientation();
-    dispatcher.dispatch('orientation', orientation);
-  }
-  // return the orientation, portait or landscape
-  function detectDeviceOrientation(){
-    switch(window.orientation) {
-			case 90:
-			case -90:
-				return 'lanscape';
-			break;
-
-			case 0:
-			case 180:
-			default:
-				return 'portait';
-		}
-	}
-  // return plugin API
-  // getOrientation returns either landscape or portait
-  // dispose removes the listener
-  return {
-    getOrientation: function() {
-      return orientation;
-    },
-    dispose: function() {
-      window.removeEventListener('orientationchange', handler);
-    }
-  }
+// plugin to add callbacks when the dom is ready
+var ReadyPlugin = function(instance) {
+  // ready function to add callbacks when the DOM is loaded (https://github.com/ded/domready)
+  var ready=function(){function l(b){for(k=1;b=a.shift();)b()}var b,a=[],c=!1,d=document,e=d.documentElement,f=e.doScroll,g="DOMContentLoaded",h="addEventListener",i="onreadystatechange",j="readyState",k=/^loade|c/.test(d[j]);return d[h]&&d[h](g,b=function(){d.removeEventListener(g,b,c),l()},c),f&&d.attachEvent(i,b=function(){/^c/.test(d[j])&&(d.detachEvent(i,b),l())}),f?function(b){self!=top?k?b():a.push(b):function(){try{e.doScroll("left")}catch(a){return setTimeout(function(){ready(b)},50)}b()}()}:function(b){k?b():a.push(b)}}();
+  // add the ready function to the prototype of the soma.js application for a direct use
+  instance.constructor.prototype.ready = ready;
 };
 
+// auto-registration, the plugin will be instantiated automatically by soma.js
+if (soma.plugins && soma.plugins.add) {
+  soma.plugins.add(ReadyPlugin);
+}
+
 // view
-var View = function(target, dispatcher, orientation) {
-  // display the orientation when the view is created
-  updateOrientation(orientation.getOrientation());
-  // listen to the event dispatched by the plugin
-  dispatcher.addEventListener('orientation', function(event) {
-    // display the orientation when a change happened
-    updateOrientation(event.params);
-  })
-  // display the orientation in the DOM Element
-  function updateOrientation(value) {
-    target.innerHTML = 'Current orientation: ' + value;
-  }
+var View = function(target, instance) {
+  instance.ready(function() {
+    target.innerHTML = 'DOM is loaded';
+  });
 };
 
 var Application = soma.Application.extend({
   init: function() {
-    // create the plugin
-    var plugin = this.createPlugin(OrientationPlugin);
-    // create a mapping rule
-    this.injector.mapValue('orientation', plugin);
+    // use the plugin
+    this.ready(function() {
+      console.log('DOM READY');
+    });
   },
   start: function() {
     // create a view

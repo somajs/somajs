d9e99b93ca2f226b32bb7b9b42537b617b469735
Romuald Quantin <romu@soundstep.com>
2013-05-01 14:28:25 +0100

    demos refactoring

--- demos/todo-light/js/lib/soma-template.js
+++ demos/todo-light/js/lib/soma-template.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 soma.template = soma.template || {};
-soma.template.version = "0.1.1";
+soma.template.version = "0.1.7";
 
 var errors = soma.template.errors = {
 	TEMPLATE_STRING_NO_ELEMENT: "Error in soma.template, a string template requirement a second parameter: an element target - soma.template.create('string', element)",
@@ -49,7 +49,8 @@ var attributes = settings.attributes = {
 	multiple: "data-multiple",
 	readonly: "data-readonly",
 	selected: "data-selected",
-	template: "data-template"
+	template: "data-template",
+	html: "data-html"
 };
 
 var vars = settings.vars = {
@@ -60,6 +61,7 @@ var vars = settings.vars = {
 var events = settings.events = {};
 settings.eventsPrefix = 'data-';
 var eventsString = 'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave keydown keyup focus blur change select selectstart scroll copy cut paste mousewheel keypress error contextmenu input textinput drag dragenter dragleave dragover dragend dragstart dragover drop load submit reset search resize beforepaste beforecut beforecopy';
+eventsString += ' touchstart touchend touchmove touchenter touchleave touchcancel gesturestart gesturechange gestureend';
 var eventsArray = eventsString.split(' ');
 var i = -1, l = eventsArray.length;
 while(++i < l) {
@@ -82,6 +84,7 @@ var regex = {
 };
 
 var ie = (function(){
+	if (typeof document !== 'object') return undefined;
 	var undef,
 		v = 3,
 		div = document.createElement('div'),
@@ -126,9 +129,7 @@ function isExpFunction(value) {
 	return !!value.match(regex.func);
 }
 function childNodeIsTemplate(node) {
-	if (!node || !isElement(node.element)) return false;
-	if (node.parent && templates.get(node.element)) return true;
-	return false;
+	return node && node.parent && templates.get(node.element);
 }
 function escapeRegExp(str) {
 	return str.replace(regex.escape, "\\$&");
@@ -190,7 +191,7 @@ function removeClass(elm, className) {
 	removeClass(elm, className);
 }
 // jquery contains
-var contains = document.documentElement.contains ?
+var contains = typeof document !== 'object' ? function(){} : document.documentElement.contains ?
 	function( a, b ) {
 		var adown = a.nodeType === 9 ? a.documentElement : a,
 			bup = b && b.parentNode;
@@ -208,10 +209,14 @@ var contains = document.documentElement.contains ?
 			}
 			return false;
 		};
-function HashMap(){
-	var uuid = function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b;}
-	var data = {};
-	var getKey = function(target) {
+
+
+function HashMap() {
+	var items = {};
+	var id = 1;
+	//var uuid = function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b;}
+	function uuid() { return ++id; };
+	function getKey(target) {
 		if (!target) return;
 		if (typeof target !== 'object') return target;
 		var result;
@@ -221,25 +226,26 @@ function HashMap(){
 		} catch(err){};
 		return result;
 	}
-	return {
-		put: function(key, value) {
-			data[getKey(key)] = value;
-		},
-		get: function(key) {
-			return data[getKey(key)];
-		},
-		remove: function(key) {
-			delete data[getKey(key)];
-		},
-		getData: function() {
-			return data;
-		},
-		dispose: function() {
-			for (var k in data) {
-				data[k] = null;
-				delete data[k];
-			}
+	this.remove = function(key) {
+		delete items[getKey(key)];
+	}
+	this.get = function(key) {
+		return items[getKey(key)];
+	}
+	this.put = function(key, value) {
+		items[getKey(key)] = value;
+	}
+	this.has = function(key) {
+		return typeof items[getKey(key)] !== 'undefined';
+	}
+	this.getData = function() {
+		return items;
+	}
+	this.dispose = function() {
+		for (var key in items) {
+			delete items[key];
 		}
+		this.length = 0;
 	}
 }
 
@@ -297,8 +303,7 @@ function getValue(scope, pattern, pathString, params, getFunction, getParams, pa
 	// find params
 	var paramsValues = [];
 	if (!paramsFound && params) {
-		var j = -1, jl = params.length;
-		while (++j < jl) {
+		for (var j = 0, jl = params.length; j < jl; j++) {
 			paramsValues.push(getValueFromPattern(scope, params[j]));
 		}
 	}
@@ -314,8 +319,7 @@ function getValue(scope, pattern, pathString, params, getFunction, getParams, pa
 	var path = scopeTarget;
 	var pathParts = pathString.split(/\.|\[|\]/g);
 	if (pathParts.length > 0) {
-		var i = -1, l = pathParts.length;
-		while (++i < l) {
+		for (var i = 0, l = pathParts.length; i < l; i++) {
 			if (pathParts[i] !== "") {
 				path = path[pathParts[i]];
 			}
@@ -358,6 +362,7 @@ function getNodeFromElement(element, scope, isRepeaterDescendant) {
 	node.previousSibling = element.previousSibling;
 	node.nextSibling = element.nextSibling;
 	var attributes = [];
+	var eventsArray = [];
 	for (var attr, name, value, attrs = element.attributes, j = 0, jj = attrs && attrs.length; j < jj; j++) {
 		attr = attrs[j];
 		if (attr.specified) {
@@ -366,12 +371,17 @@ function getNodeFromElement(element, scope, isRepeaterDescendant) {
 			if (name === settings.attributes.skip) {
 				node.skip = (value === "" || value === "true");
 			}
-			if (!isRepeaterDescendant && name === settings.attributes.repeat) {
+			if (name === settings.attributes.html) {
+				node.html = (value === "" || value === "true");
+			}
+			if (name === settings.attributes.repeat && !isRepeaterDescendant) {
 				node.repeater = value;
 			}
 			if (
 				hasInterpolation(name + ':' + value) ||
 					name === settings.attributes.repeat ||
+					name === settings.attributes.skip ||
+					name === settings.attributes.html ||
 					name === settings.attributes.show ||
 					name === settings.attributes.hide ||
 					name === settings.attributes.href ||
@@ -384,13 +394,16 @@ function getNodeFromElement(element, scope, isRepeaterDescendant) {
 				) {
 				attributes.push(new Attribute(name, value, node));
 			}
-			if (events[name]) {
-				node.addEvent(events[name], value);
+			if (events[name] && !isRepeaterDescendant) {
+				eventsArray.push({name:events[name], value:value});
 				attributes.push(new Attribute(name, value, node));
 			}
 		}
 	}
 	node.attributes = attributes;
+	for (var i = 0, l = eventsArray.length; i < l; i++) {
+		node.addEvent(eventsArray[i].name, eventsArray[i].value);
+	}
 	return node;
 }
 
@@ -420,11 +433,14 @@ function compile(template, element, parent, nodeTarget) {
 	// get node
 	var node;
 	if (!nodeTarget) {
-		node = getNodeFromElement(element, parent ? parent.scope : new Scope(helpersScopeObject)._createChild());
+		node = getNodeFromElement(element, parent ? parent.scope : new Scope(helpersScopeObject)._createChild(), parent && (parent.repeater || parent.isRepeaterDescendant) );
 	}
 	else {
 		node = nodeTarget;
 		node.parent = parent;
+	}
+	if (parent && (parent.repeater || parent.isRepeaterDescendant)) {
+		node.isRepeaterDescendant = true;
 	}
 	node.template = template;
 	// children
@@ -458,17 +474,15 @@ function clearScope(scope) {
 }
 
 function updateNodeChildren(node) {
-	if (childNodeIsTemplate(node) || node.repeater || !node.children) return;
-	var i = -1, l = node.children.length;
-	while (++i < l) {
+	if (node.repeater || !node.children || childNodeIsTemplate(node)) return;
+	for (var i = 0, l = node.children.length; i < l; i++) {
 		node.children[i].update();
 	}
 }
 
 function renderNodeChildren(node) {
-	if (childNodeIsTemplate(node) || !node.children) return;
-	var i = -1, l = node.children.length;
-	while (++i < l) {
+	if (!node.children || childNodeIsTemplate(node)) return;
+	for (var i = 0, l = node.children.length; i < l; i++) {
 		node.children[i].render();
 	}
 }
@@ -478,11 +492,7 @@ function renderNodeRepeater(node) {
 	var previousElement;
 	if (isArray(data)) {
 		// process array
-		var i = -1;
-		var l1 = data.length;
-		var l2 = node.childrenRepeater.length;
-		var l = l1 > l2 ? l1 : l2;
-		while (++i < l) {
+		for (var i = 0, l1 = data.length, l2 = node.childrenRepeater.length, l = l1 > l2 ? l1 : l2; i < l; i++) {
 			if (i < l1) {
 				previousElement = createRepeaterChild(node, i, data[i], vars.index, i, previousElement);
 			}
@@ -517,11 +527,14 @@ function renderNodeRepeater(node) {
 function cloneRepeaterNode(element, node) {
 	var newNode = new Node(element, node.scope._createChild());
 	if (node.attributes) {
-		var i = -1, l = node.attributes.length;
 		var attrs = [];
-		while (++i < l) {
+		for (var i = 0, l = node.attributes.length; i < l; i++) {
+			newNode.renderAsHtml = node.renderAsHtml;
 			if (node.attributes[i].name === settings.attributes.skip) {
 				newNode.skip = (node.attributes[i].value === "" || node.attributes[i].value === "true");
+			}
+			if (node.attributes[i].name === settings.attributes.html) {
+				newNode.html = (node.attributes[i].value === "" || node.attributes[i].value === "true");
 			}
 			if (node.attributes[i].name !== attributes.repeat) {
 				var attribute = new Attribute(node.attributes[i].name, node.attributes[i].value, newNode);
@@ -531,7 +544,6 @@ function cloneRepeaterNode(element, node) {
 				newNode.addEvent(events[node.attributes[i].name], node.attributes[i].value);
 			}
 		}
-		newNode.isRepeaterDescendant = true;
 		newNode.attributes = attrs;
 	}
 	return newNode;
@@ -545,7 +557,8 @@ function createRepeaterChild(node, count, data, indexVar, indexVarValue, previou
 		// can't recreate the node with a cloned element on IE7
 		// be cause the attributes are not specified annymore (attribute.specified)
 		//var newNode = getNodeFromElement(newElement, node.scope._createChild(), true);
-		var newNode = cloneRepeaterNode(newElement, node)
+		var newNode = cloneRepeaterNode(newElement, node);
+		newNode.isRepeaterChild = true;
 		newNode.parent = node.parent;
 		newNode.template = node.template;
 		node.childrenRepeater[count] = newNode;
@@ -603,6 +616,7 @@ var Node = function(element, scope) {
 	this.skip = false;
 	this.repeater = null;
 	this.isRepeaterDescendant = false;
+	this.isRepeaterChild = false;
 	this.parent = null;
 	this.children = [];
 	this.childrenRepeater = [];
@@ -610,10 +624,11 @@ var Node = function(element, scope) {
 	this.nextSibling = null;
 	this.template = null;
 	this.eventHandlers = {};
+	this.html = false;
 
 	if (isTextNode(this.element)) {
 		this.value = this.element.nodeValue;
-		this.interpolation = new Interpolation(this.value, this);
+		this.interpolation = new Interpolation(this.value, this, undefined);
 	}
 
 };
@@ -625,20 +640,17 @@ Node.prototype = {
 		this.clearEvents();
 		var i, l;
 		if (this.children) {
-			i = -1; l = this.children.length;
-			while (++i < l) {
+			for (i = 0, l = this.children.length; i < l; i++) {
 				this.children[i].dispose();
 			}
 		}
 		if (this.childrenRepeater) {
-			i = 0; l = this.childrenRepeater.length;
-			while (++i < l) {
+			for (i = 0, l = this.childrenRepeater.length; i < l; i++) {
 				this.childrenRepeater[i].dispose();
 			}
 		}
 		if (this.attributes) {
-			i = 0; l = this.attributes.length;
-			while (++i < l) {
+			for (i = 0, l = this.attributes.length; i < l; i++) {
 				this.attributes[i].dispose();
 			}
 		}
@@ -664,15 +676,13 @@ Node.prototype = {
 		var node;
 		if (element === this.element) return this;
 		else if (this.childrenRepeater.length > 0) {
-			var k = -1, kl = this.childrenRepeater.length;
-			while (++k < kl) {
+			for (var k = 0, kl = this.childrenRepeater.length; k < kl; k++) {
 				node = this.childrenRepeater[k].getNode(element);
 				if (node) return node;
 			}
 		}
 		else {
-			var i = -1, l = this.children.length;
-			while (++i < l) {
+			for (var i = 0, l = this.children.length; i < l; i++) {
 				node = this.children[i].getNode(element);
 				if (node) return node;
 			}
@@ -681,8 +691,7 @@ Node.prototype = {
 	},
 	getAttribute: function(name) {
 		if (this.attributes) {
-			var i = -1, l = this.attributes.length;
-			while (++i < l) {
+			for (var i = 0, l = this.attributes.length; i < l; i++) {
 				var att = this.attributes[i];
 				if (att.interpolationName && att.interpolationName.value === name) {
 					return att;
@@ -696,8 +705,7 @@ Node.prototype = {
 			this.interpolation.update();
 		}
 		if (isDefined(this.attributes)) {
-			var i = -1, l = this.attributes.length;
-			while (++i < l) {
+			for (var i = 0, l = this.attributes.length; i < l; i++) {
 				this.attributes[i].update();
 			}
 		}
@@ -708,20 +716,14 @@ Node.prototype = {
 		this.invalidate = true;
 		var i, l;
 		if (this.attributes) {
-			i = -1
-			l = this.attributes.length;
-			while (++i < l) {
+			for (i = 0, l = this.attributes.length; i < l; i++) {
 				this.attributes[i].invalidate = true;
 			}
 		}
-		i = -1;
-		l = this.childrenRepeater.length;
-		while (++i < l) {
+		for (i = 0, l = this.childrenRepeater.length; i < l; i++) {
 			this.childrenRepeater[i].invalidateData();
 		}
-		i = -1;
-		l = this.children.length;
-		while (++i < l) {
+		for (i = 0, l = this.children.length; i < l; i++) {
 			this.children[i].invalidateData();
 		}
 	},
@@ -731,13 +733,14 @@ Node.prototype = {
 			this.removeEvent(type);
 		}
 		var scope = this.scope;
-		var node = node;
 		var handler = function(event) {
-			var exp = new Expression(pattern, node);
+			var exp = new Expression(pattern, this.node);
 			var func = exp.getValue(scope, true);
 			var params = exp.getValue(scope, false, true);
 			params.unshift(event);
-			if (func) func.apply(null, params);
+			if (func) {
+				func.apply(null, params);
+			}
 		};
 		this.eventHandlers[type] = handler;
 		addEvent(this.element, type, handler);
@@ -754,14 +757,12 @@ Node.prototype = {
 			}
 		}
 		if (this.children) {
-			var k = -1, kl = this.children.length;
-			while (++k < kl) {
+			for (var k = 0, kl = this.children.length; k < kl; k++) {
 				this.children[k].clearEvents();
 			}
 		}
 		if (this.childrenRepeater) {
-			var f = -1, fl = this.childrenRepeater.length;
-			while (++f < fl) {
+			for (var f = 0, fl = this.childrenRepeater.length; f < fl; f++) {
 				this.childrenRepeater[f].clearEvents();
 			}
 		}
@@ -771,12 +772,16 @@ Node.prototype = {
 		if (this.invalidate) {
 			this.invalidate = false;
 			if (isTextNode(this.element)) {
-				this.value = this.element.nodeValue = this.interpolation.render();
+				if (this.parent && this.parent.html) {
+					this.value = this.parent.element.innerHTML = this.interpolation.render();
+				}
+				else {
+					this.value = this.element.nodeValue = this.interpolation.render();
+				}
 			}
 		}
 		if (this.attributes) {
-			var i = -1, l = this.attributes.length;
-			while (++i < l) {
+			for (var i = 0, l = this.attributes.length; i < l; i++) {
 				this.attributes[i].render();
 			}
 		}
@@ -822,6 +827,8 @@ Attribute.prototype = {
 			this.previousName = this.name;
 			this.name = isDefined(this.interpolationName.render()) ? this.interpolationName.render() : this.name;
 			this.value = isDefined(this.interpolationValue.render()) ? this.interpolationValue.render() : this.value;
+
+
 			if (this.name === attributes.src) {
 				renderSrc(this.name, this.value);
 			}
@@ -829,7 +836,7 @@ Attribute.prototype = {
 				renderHref(this.name, this.value);
 			}
 			else {
-				if (this.node.isRepeaterDescendant && ie === 7) {
+				if (this.node.isRepeaterChild && ie === 7) {
 					// delete attributes on cloned elements crash IE7
 				}
 				else {
@@ -841,7 +848,7 @@ Attribute.prototype = {
 						this.node.element.className = "";
 					}
 					else {
-						if (this.node.isRepeaterDescendant && ie === 7) {
+						if (this.node.isRepeaterChild && ie === 7) {
 							// delete attributes on cloned elements crash IE7
 						}
 						else {
@@ -927,8 +934,7 @@ var Interpolation = function(value, node, attribute) {
 	this.expressions = [];
 	var parts = this.value.match(regex.sequence);
 	if (parts) {
-		var i = -1, l = parts.length;
-		while (++i < l) {
+		for (var i = 0, l = parts.length; i < l; i++) {
 			if (parts[i].match(regex.token)) {
 				var exp = new Expression(trimTokens(parts[i]), this.node, this.attribute);
 				this.sequence.push(exp);
@@ -947,8 +953,7 @@ Interpolation.prototype = {
 	},
 	dispose: function() {
 		if (this.expressions) {
-			var i = -1, l = this.expressions.length;
-			while (++i < l) {
+			for (var i = 0, l = this.expressions.length; i < l; i++) {
 				this.expressions[i].dispose();
 			}
 		}
@@ -967,8 +972,7 @@ Interpolation.prototype = {
 	render: function() {
 		var rendered = "";
 		if (this.sequence) {
-			var i = -1, l = this.sequence.length;
-			while (++i < l) {
+			for (var i = 0, l = this.sequence.length; i < l; i++) {
 				var val = "";
 				if (isExpression(this.sequence[i])) val = this.sequence[i].value;
 				else val = this.sequence[i];
@@ -1218,7 +1222,7 @@ function clearEvents(element) {
 }
 
 
-if (settings.autocreate) {
+if (settings.autocreate && typeof document === 'object') {
 	// https://github.com/ded/domready
 	var ready=function(){function l(b){for(k=1;b=a.shift();)b()}var b,a=[],c=!1,d=document,e=d.documentElement,f=e.doScroll,g="DOMContentLoaded",h="addEventListener",i="onreadystatechange",j="readyState",k=/^loade|c/.test(d[j]);return d[h]&&d[h](g,b=function(){d.removeEventListener(g,b,c),l()},c),f&&d.attachEvent(i,b=function(){/^c/.test(d[j])&&(d.detachEvent(i,b),l())}),f?function(b){self!=top?k?b():a.push(b):function(){try{e.doScroll("left")}catch(a){return setTimeout(function(){ready(b)},50)}b()}()}:function(b){k?b():a.push(b)}}();
 	var parse = function(element) {
@@ -1282,7 +1286,6 @@ function createTemplate(source, target) {
 }
 
 function getTemplate(element) {
-	if (!isElement(element)) return null;
 	return templates.get(element);
 }
 

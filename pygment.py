46ebc483db1153e9ef0ea4ab441b46b831d8f5b3
Romuald Quantin <romu@soundstep.com>
2013-12-10 12:20:53 +0000

    dom mediator in progress

--- pygment.py
+++ pygment.py
@@ -2,24 +2,21 @@ from pygments import highlight
 from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<script>
-var TitleView = function(target, dispatcher, titleValue) {
-  // titleValue has been injected when creating the mediator
-  target.innerHTML = titleValue;
+code = """<div data-mediator="Mediator"></div>
+<div data-mediator="Mediator"></div>
+<div data-mediator="Mediator"></div>
+<script>
+var Mediator = function(target) {
+  target.innerHTML = "I'm a DOM Element with a mediator.";
 };
-
 var Application = soma.Application.extend({
   init: function() {
-    this.injector.mapValue('mapped', 'This is a title');
-    // data that will be injected into the mediator
-    var mediatorData = {
-      titleValue: 'mapped'
-    };
-    // create a mediator that represents a DOM Element
-    this.mediators.create(TitleView, document.getElementById('title'), mediatorData);
+    // set mediator mapping
+    this.mediators.map('Mediator', Mediator);
+    // observe an element
+    this.mediators.observe(document.body);
   }
 });
-
 var app = new Application();
 </script>
 """

2432502eaf9f55084e3ea7e639c86c2cdcceb2f9
Romuald Quantin <romu@soundstep.com>
2013-12-10 14:59:29 +0000

    dom mediator doc in progress

--- pygment.py
+++ pygment.py
@@ -2,22 +2,37 @@ from pygments import highlight
 from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<div data-mediator="Mediator"></div>
-<div data-mediator="Mediator"></div>
-<div data-mediator="Mediator"></div>
+code = """<div select="SelectMediator"></div>
+<div choose="ChooseMediator"></div>
+<div pick="PickMediator"></div>
+
 <script>
-var Mediator = function(target) {
-  target.innerHTML = "I'm a DOM Element with a mediator.";
+
+var SelectMediator = function(target) {
+  target.innerHTML = "I'm a mediator, my name is: SelectMediator";
 };
+
+var ChooseMediator = function(target) {
+  target.innerHTML = "I'm a mediator, my name is: ChooseMediator";
+};
+
+var PickMediator = function(target) {
+  target.innerHTML = "I'm a mediator, my name is: PickMediator";
+};
+
 var Application = soma.Application.extend({
   init: function() {
     // set mediator mapping
-    this.mediators.map('Mediator', Mediator);
+    this.mediators.describe('select').map('SelectMediator', SelectMediator);
+    this.mediators.describe('choose').map('ChooseMediator', ChooseMediator);
+    this.mediators.describe('pick').map('PickMediator', PickMediator);
     // observe an element
     this.mediators.observe(document.body);
   }
 });
+
 var app = new Application();
+
 </script>
 """
 

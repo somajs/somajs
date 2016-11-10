63f5e63302448fc50016bb75e11e2eb87cf66b8b
soundstep <romu@soundstep.com>
2016-11-10 08:54:24 +0000

    Update documentation.

--- pygment.py
+++ pygment.py
@@ -3,7 +3,9 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<script>
-bower install soma.js
+var soma = require('soma.js');
+var template = require('soma-template');
+soma.plugins.add(template.Plugin);
 </script>
 """
 
